3a02bb528d091f59f633c5ac2d5d016b2451a534
Alexey Roytman <roytman@il.ibm.com>
2024-06-20 10:29:33 +0300

    add exithandler example

---
+++ transforms/universal/noop/kfp_ray/exit.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Copyright 2019-2023 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Pipeline using ExitHandler."""
+
+from kfp import local
+from kfp import dsl
+from kfp.dsl import component
+
+local.init(runner=local.DockerRunner())
+
+@component(base_image='python:3.10')
+def print_op(message: str):
+    """Prints a message."""
+    print(message)
+    sys.exit(1)
+
+
+@component(base_image='python:3.10')
+def fail_op(message: str):
+    """Fails."""
+    import sys
+    print(message)
+    sys.exit(1)
+
+
+@dsl.pipeline(name='pipeline-with-exit-handler')
+def pipeline_exit_handler(message: str = 'Hello World!'):
+
+    exit_task = print_op(message='Exit handler has worked!')
+
+    with dsl.ExitHandler(exit_task):
+        fail_op(message='Task failed.')
+
+
+pipeline_exit_handler()
