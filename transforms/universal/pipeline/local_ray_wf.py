f5c6ac034c73c5d606fea6f6b006dc18a878a761
Alexey Roytman <roytman@il.ibm.com>
2024-06-30 15:58:17 +0300

    fix local ray path

--- transforms/universal/pipeline/local_ray_wf.py
+++ transforms/universal/pipeline/local_ray_wf.py
@@ -7,19 +7,19 @@ local.init(runner=local.DockerRunner())
 def doc_id():
     import subprocess
     import sys
-    subprocess.run([sys.executable, "doc_id_local_ray.py"], check=True)
+    subprocess.run([sys.executable, "src/doc_id_local_ray.py"], check=True)
 
 @dsl.component(base_image="quay.io/dataprep1/data-prep-kit/ededup-ray:0.2.0")
 def ededup():
     import subprocess
     import sys
-    subprocess.run([sys.executable, "ededup_local_ray.py"], check=True)
+    subprocess.run([sys.executable, "src/ededup_local_ray.py"], check=True)
 
 @dsl.component(base_image="quay.io/dataprep1/data-prep-kit/fdedup-ray:0.2.0")
 def fdedup():
     import subprocess
     import sys
-    subprocess.run([sys.executable, "fdedup_local_ray.py"], check=True)
+    subprocess.run([sys.executable, "src/fdedup_local_ray.py"], check=True)
 
 @dsl.pipeline
 def test_pipeline():
