2e48310335799eafd3d1204945e5393c647d2877
Alexey Roytman <roytman@il.ibm.com>
2025-03-28 15:51:58 +0300

    organize secrets
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>

--- transforms/language/lang_id/kfp_ray/lang_id_wf.py
+++ transforms/language/lang_id/kfp_ray/lang_id_wf.py
@@ -35,7 +35,7 @@ task_image = "quay.io/dataprep1/data-prep-kit/lang_id-ray:latest"
 EXEC_SCRIPT_NAME: str = "-m dpk_lang_id.ray.transform"
 
 # components
-base_kfp_image = "quay.io/dataprep1/data-prep-kit/kfp-data-processing:latest"
+base_kfp_image = "quay.io/dataprep1/data-prep-kit/kfp-data-processing:3.0"
 
 # path to kfp component specifications files
 component_spec_path = os.getenv("KFP_COMPONENT_SPEC_PATH", DEFAULT_KFP_COMPONENT_SPEC_PATH)
